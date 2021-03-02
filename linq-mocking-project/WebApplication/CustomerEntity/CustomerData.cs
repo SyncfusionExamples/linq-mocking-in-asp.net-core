d9a0f804dec49475f35cacd12cda0c762613c1a8
Priyankasubbaraj <34092709+Priyankasubbaraj@users.noreply.github.com>
2021-03-02 15:21:44 +0530

    Create CustomerData.cs

---
+++ linq-mocking-project/WebApplication/CustomerEntity/CustomerData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication.CustomerEntity
+{
+    public partial class CustomerData
+    {
+        public int Id { get; set; }
+        public string email { get; set; }
+    }
+}
